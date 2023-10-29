234b4c92094615d1d60205080f3bbd2cb2b4be9e
lilyeap <lileapy@gmail.com>
2023-10-28 23:39:44 -0500

    finished EdgeDetect

--- Library.fs
+++ Library.fs
@@ -48,6 +48,7 @@ module Operations =
                     (threshold:int) = 
     let checkPixel (pix: int) =
       if pix > threshold then 255 else 0
+
     image |> List.map (fun row -> row |> List.map (fun (r, g, b) ->  (checkPixel r, checkPixel g, checkPixel b)))
 
   // description:
@@ -66,33 +67,50 @@ module Operations =
       match img with
       | [] -> []
       | row :: next -> List.rev row :: reverseRows next
+
     reverseRows image
 
-  let rec detectRow row threshold =
-    match row with
-    | [] -> []
-    | pixel::nextPixel::xs -> 
-        [if sqrt (float ((fun (r1, g1, b1) (r2, g2, b2) -> (r2 - r1) * (r2 - r1) + (g2 - g1) * (g2 - g1) + (b2 - b1) * (b2 - b1)) pixel nextPixel)) > float threshold then (0, 0, 0) else (255, 255, 255)] 
-        @ (detectRow (nextPixel::xs) threshold)
-    | [lastPixel] -> [lastPixel]
+  // description:
+  // detects if there is an edge based on a threshold and will transform the image accordingly
 
+  // breakdown:
+  // image is passed to a helper function called edgeDetectRec
+  // iterate through the image two rows at a time, which is passed to another helper function called edgeHelper
+  // edge helper will use a recursive function called comparePixels and accumulate a new row
+  // in comparePixels, we make sure there is a pixel to the right and below
+  // if there are pixels adjacent, then we compare the pixels based on the threshold and change them accordingly
+  // we call the comparePixels again until we reach the end, where we reverse the resulting row
   let rec EdgeDetect (width:int)
-               (height:int)
-               (depth:int)
-               (image:(int*int*int) list list)
-               (threshold:int) = 
-    match image with
-    | [] -> []
-    | row::rest -> [detectRow row threshold] @ (EdgeDetect width height depth rest threshold)
+                   (height:int)
+                   (depth:int)
+                   (image:(int * int * int) list list)
+                   (threshold:int) = 
+    let distance (r1:int, g1:int, b1:int) (r2:int, g2:int, b2:int) =
+      sqrt(float(r2 - r1) * float(r2 - r1) + float(g2 - g1) * float(g2 - g1) + float(b2 - b1) * float(b2 - b1))
 
+    let edgeHelper row1 row2 threshold =
+      let rec comparePixels row1 row2 acc =
+        match row1, row2 with
+        | hd1::next1::rest1, hd2::rest2 ->  // ensure at least two elements in row1 & one element in row2
+            comparePixels (next1::rest1) rest2 ((if (distance hd1 next1) > float threshold || (distance hd1 hd2) > float threshold then (0, 0, 0) else (255, 255, 255)) :: acc)
+        | _ -> List.rev acc
+      comparePixels row1 row2 []
+        
+    let rec edgeDetectRec rows =
+      match rows with
+      | row1::(row2::_ as tail) ->
+          (edgeHelper row1 row2 threshold) :: edgeDetectRec tail
+      | _ -> []
+
+    edgeDetectRec image
 
   // description:
-  // rotates an image right 90 degrees
+  // rotates an image right 90 degrees by transposing it first and then reversing the whole image
 
   // breakdown:
   // image is passed to a recursive helper function called transpose
-  // in transpose, the head of the list will 
-  // 
+  // in transpose, the head of the each row will be made a row itself
+  // then, we repeat for the next elements
   // the list that is returned from the recursive helper will be passed to a higher order function, List.map
   // List.map will reverse each row of tuples
   let rec RotateRight90 (width:int)
@@ -100,11 +118,11 @@ module Operations =
                         (depth:int)
                         (image:(int*int*int) list list) = 
     let rec transpose lst =
-        match lst with
-        | [] -> []
-        | []::_ -> []
-        | _ -> (List.map List.head lst) :: transpose (List.map List.tail lst)
+      match lst with
+      | [] -> []
+      | []::_ -> [] // in case the row is empty
+      | _ -> (List.map List.head lst) :: transpose (List.map List.tail lst)
+
     image |> transpose |> List.map List.rev
-    // image |> transpose 
     
 
